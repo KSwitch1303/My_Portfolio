9d76905d95c7dc5a2079fafd01083723524d475d
Favor Misongo <ebimieteimisongo@gmail.com>
2024-03-12 17:44:04 +0100

    master commit

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white">
-      <div className="container p-12 flex justify-between">
-        <span>K-Switch</span>
-        <p className="text-slate-600">Developed by K-Switch</p>
+    <footer className="footer border z-10 border-t-[#0000] border-l-transparent border-r-transparent text-white">
+      <div className="container p-12 justify-between">
+        {/* <span>K-Switch</span> */}
+        <p className="text-slate-600">©Developed by K-Switch™</p>
       </div>
     </footer>
   );
