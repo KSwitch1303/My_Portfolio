9ffb64af93f02a0fe37f4e377f14206e182e197e
Favor Misongo <ebimieteimisongo@gmail.com>
2024-08-04 20:24:18 +0100

    master commit

--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -1,25 +1,55 @@
 import { NextResponse } from "next/server";
 import { Resend } from "resend";
 
+const nodemailer = require('nodemailer');
 const resend = new Resend('re_h1Fx1BiV_5RP1tLqnAmYeHqnNdcuBHrdF');
 const fromEmail = 'onboarding@resend.dev';
 const toEmail = 'ebimieteimisongo@gmail.com';
+const nodemailerPassword = "urfc muoc iutl qrxy";
 export async function POST(req, res) {
   const { email, subject, message } = await req.json();
-  try {
-    const data = await resend.emails.send({
-      from: fromEmail,
-      to: [fromEmail, toEmail],
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'ebimieteimisongo@gmail.com',
+      pass: nodemailerPassword
+    }
+  });
+  const sendMail = async (mail, name) => {
+    let mailOptions = {
+      from: email,
+      to: 'ebimieteimisongo@gmail.com',
       subject: subject,
-      react: (
-        <>
-          <h1>{subject}</h1>
+      html: `<h1>${subject}</h1>
           <p>New message submitted:</p>
-          <p>From {email}</p>
-          <p>{message}</p>
-        </>
-      ),
+          <p>From ${email}</p>
+          <p>${message}</p>`
+    };
+    // mailOptions.to = mail
+    // receiverName = name
+    transporter.sendMail(mailOptions, function(error, info){
+      if (error) {
+        console.log(error);
+      } else {
+        console.log('Email sent: ' + info.response);
+      }
     });
+  }
+  try {
+    sendMail(req.body.email, req.body.name)
+    // const data = await resend.emails.send({
+    //   from: fromEmail,
+    //   to: [fromEmail, toEmail],
+    //   subject: subject,
+    //   react: (
+    //     <>
+    //       <h1>{subject}</h1>
+    //       <p>New message submitted:</p>
+    //       <p>From {email}</p>
+    //       <p>{message}</p>
+    //     </>
+    //   ),
+    // });
     return NextResponse.json(data);
   } catch (error) {
     return NextResponse.json({ error });
