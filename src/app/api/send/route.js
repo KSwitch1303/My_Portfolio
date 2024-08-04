554b72630695574de79996e7a43d661779021204
Favor Misongo <ebimieteimisongo@gmail.com>
2024-08-04 20:31:53 +0100

    master commit

--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -1,10 +1,6 @@
 import { NextResponse } from "next/server";
-import { Resend } from "resend";
 
 const nodemailer = require('nodemailer');
-const resend = new Resend('re_h1Fx1BiV_5RP1tLqnAmYeHqnNdcuBHrdF');
-const fromEmail = 'onboarding@resend.dev';
-const toEmail = 'ebimieteimisongo@gmail.com';
 const nodemailerPassword = "urfc muoc iutl qrxy";
 export async function POST(req, res) {
   const { email, subject, message } = await req.json();
@@ -25,8 +21,6 @@ export async function POST(req, res) {
           <p>From ${email}</p>
           <p>${message}</p>`
     };
-    // mailOptions.to = mail
-    // receiverName = name
     transporter.sendMail(mailOptions, function(error, info){
       if (error) {
         console.log(error);
@@ -37,19 +31,6 @@ export async function POST(req, res) {
   }
   try {
     sendMail()
-    // const data = await resend.emails.send({
-    //   from: fromEmail,
-    //   to: [fromEmail, toEmail],
-    //   subject: subject,
-    //   react: (
-    //     <>
-    //       <h1>{subject}</h1>
-    //       <p>New message submitted:</p>
-    //       <p>From {email}</p>
-    //       <p>{message}</p>
-    //     </>
-    //   ),
-    // });
     return NextResponse.json(data);
   } catch (error) {
     return NextResponse.json({ error });
